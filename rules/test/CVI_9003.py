92fe12b139f90af9082d6288b155ff6efd79bbb7
LoRexxar <lorexxar@gmail.com>
2018-07-20 15:25:47 +0800

    merge

---
+++ rules/test/CVI_9003.py
@@ -0,0 +1,28 @@
+
+class CVI_9003():
+    """
+    rule class
+
+    """
+    def __init__(self):
+
+        self.svid = 9003
+        self.language = "test"
+        self.author = "lorexxar"
+        self.vulnerability = "test"
+        self.description = "saaaa"
+
+        self.status = True
+
+        self.match_mode = "3"
+        self.match = "test"
+        self.vul_function = None
+
+    def main(self, regex_string):
+        """
+        regex string input
+        :regex_string: regex match string
+        :return:
+        """
+        pass
+
