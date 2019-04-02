ebd8ba8517a45d5d912d9a8489316881280ca2e7
LoRexxar <lorexxar@gmail.com>
2019-04-02 14:13:21 +0800

    try to fix bug for max

--- cobra/utils.py
+++ cobra/utils.py
@@ -393,27 +393,27 @@ class Tool:
             self.find = 'find'
 
 
-        if 'darwin' == sys.platform:
-            ggrep = ''
-            gfind = ''
-            for root, dir_names, file_names in os.walk('/usr/local/Cellar/grep'):
-                for filename in file_names:
-                    if 'ggrep' == filename or 'grep' == filename:
-                        ggrep = os.path.join(root, filename)
-            for root, dir_names, file_names in os.walk('/usr/local/Cellar/findutils'):
-                for filename in file_names:
-                    if 'gfind' == filename:
-                        gfind = os.path.join(root, filename)
-            if ggrep == '':
-                logger.critical("brew install ggrep pleases!")
-                sys.exit(0)
-            else:
-                self.grep = ggrep
-            if gfind == '':
-                logger.critical("brew install findutils pleases!")
-                sys.exit(0)
-            else:
-                self.find = gfind
+        # if 'darwin' == sys.platform:
+        #     ggrep = ''
+        #     gfind = ''
+        #     for root, dir_names, file_names in os.walk('/usr/local/Cellar/grep'):
+        #         for filename in file_names:
+        #             if 'ggrep' == filename or 'grep' == filename:
+        #                 ggrep = os.path.join(root, filename)
+        #     for root, dir_names, file_names in os.walk('/usr/local/Cellar/findutils'):
+        #         for filename in file_names:
+        #             if 'gfind' == filename:
+        #                 gfind = os.path.join(root, filename)
+        #     if ggrep == '':
+        #         logger.critical("brew install ggrep pleases!")
+        #         sys.exit(0)
+        #     else:
+        #         self.grep = ggrep
+        #     if gfind == '':
+        #         logger.critical("brew install findutils pleases!")
+        #         sys.exit(0)
+        #     else:
+        #         self.find = gfind
 
 
 def secure_filename(filename):
