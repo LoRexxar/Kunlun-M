476e6c8a6507d7643c940bc644a0ab89d624d0d2
LoRexxar <lorexxar@gmail.com>
2019-06-19 13:59:53 +0800

    try to add crx audit

--- cobra/pretreatment.py
+++ cobra/pretreatment.py
@@ -10,13 +10,44 @@ from phply.phplex import lexer  # 词法分析
 from phply.phpparse import make_parser  # 语法分析
 from phply import phpast as php
 from .log import logger
+from .const import ext_dict
 
 import os
+import json
 import codecs
 import traceback
+import zipfile
 
 
-could_ast_pase_lans = ["php"]
+could_ast_pase_lans = ["php", "chromeext"]
+
+
+def un_zip(target_path):
+    """
+    解压缩目标压缩包
+    :return: 
+    """
+
+    logger.info("[Pre][Unzip] Upzip file {}...".format(target_path))
+
+    if not os.path.isfile(target_path):
+        logger.warn("[Pre][Unzip] Target file {} is't exist...pass".format(target_path))
+        return False
+
+    zip_file = zipfile.ZipFile(target_path)
+    target_file_path = target_path+"_files/"
+
+    if os.path.isdir(target_file_path):
+        logger.debug("[Pre][Unzip] Target files {} is exist...continue".format(target_file_path))
+        return target_file_path
+    else:
+        os.mkdir(target_file_path)
+
+    for names in zip_file.namelist():
+        zip_file.extract(names, target_file_path)
+    zip_file.close()
+
+    return target_file_path
 
 
 class Pretreatment:
@@ -47,7 +78,7 @@ class Pretreatment:
 
         for fileext in self.file_list:
 
-            if ".php" == fileext[0]:
+            if fileext[0] in ext_dict['php']:
                 # 下面是对于php文件的处理逻辑
                 for filepath in fileext[1]['list']:
                     all_nodes = []
@@ -80,20 +111,58 @@ class Pretreatment:
 
                             self.define_dict[define_params[0].node] = define_params[1].node
 
+            elif fileext[0] in ext_dict['chromeext']:
+                child_files = []
+
+                # 针对chrome 拓展的预处理
+                # 需要提取其中的js和html？
+                for filepath in fileext[1]['list']:
+                    filepath = os.path.join(self.target_directory, filepath)
+                    self.pre_result[filepath] = {}
+                    self.pre_result[filepath]['language'] = 'chromeext'
+
+                    # 首先想办法解压crx
+                    target_files_path = un_zip(filepath)
+                    self.pre_result[filepath]['target_files_path'] = target_files_path
+
+                    # 分析manifest.json
+                    manifest_path = os.path.join(target_files_path, "manifest.json")
+                    relative_path = target_files_path.split(self.target_directory)[-1]
+
+                    if relative_path.startswith('\\') or relative_path.startswith("/"):
+                        relative_path = relative_path[1:]
+
+                    if os.path.isfile(manifest_path):
+                        fi = codecs.open(manifest_path, "r", encoding='utf-8', errors='ignore')
+                        manifest_content = fi.read()
+                        manifest = json.loads(manifest_content)
+
+                        self.pre_result[filepath]["manifest"] = manifest
+
+                        if "content_scripts" in manifest:
+                            for script in manifest["content_scripts"]:
+                                child_files.extend([os.path.join(relative_path, js) for js in script['js']])
+
+                        self.pre_result[filepath]["child_files"] = child_files
+                    else:
+                        logger.warning("[Pretreatment][Chrome Ext] File {} parse error...".format(target_files_path))
+                        continue
+
     def get_nodes(self, filepath):
         filepath = os.path.normpath(filepath)
 
         if filepath in self.pre_result:
             return self.pre_result[filepath]['ast_nodes']
 
-        elif self.target_directory + filepath in self.pre_result:
-            return self.pre_result[self.target_directory + filepath]['ast_nodes']
+        elif os.path.join(self.target_directory, filepath) in self.pre_result:
+            return self.pre_result[os.path.join(self.target_directory, filepath)]['ast_nodes']
 
         else:
             logger.warning("[AST] file {} parser not found...".format(filepath))
             return False
 
     def get_content(self, filepath):
+        filepath = os.path.normpath(filepath)
 
         if filepath in self.pre_result:
             return self.pre_result[filepath]['content']
@@ -102,8 +171,27 @@ class Pretreatment:
             logger.warning("[AST] file {} parser not found...".format(filepath))
             return False
 
-    def get_object(self):
-        return self
+    def get_object(self, filepath):
+        filepath = os.path.normpath(filepath)
+
+        if filepath in self.pre_result:
+            return self.pre_result[filepath]
+        else:
+            logger.warning("[AST] file {} object not found...".format(filepath))
+            return False
+
+    def get_child_files(self, filepath):
+        filepath = os.path.normpath(filepath)
+
+        if filepath in self.pre_result and "child_files" in self.pre_result[filepath]:
+            return self.pre_result[filepath]['child_files']
+
+        elif os.path.join(self.target_directory, filepath) in self.pre_result and "child_files" in self.pre_result[os.path.join(self.target_directory, filepath)]:
+            return self.pre_result[os.path.join(self.target_directory, filepath)]['child_files']
+
+        else:
+            logger.warning("[AST] file {} object or child files not found...".format(filepath))
+            return False
 
     def get_define(self, define_name):
         if define_name in self.define_dict:
