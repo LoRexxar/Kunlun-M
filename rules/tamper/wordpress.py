e96388b467de3e2192379d0c85b5d42d7c0d1532
LoRexxar <lorexxar@gmail.com>
2020-08-10 17:54:14 +0800

    new tamper

---
+++ rules/tamper/wordpress.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+    wordpress
+    ~~~~
+
+    tamper for wordpress
+
+    :author:    LoRexxar <LoRexxar@gmail.com>
+    :homepage:  https://github.com/LoRexxar/Kunlun-M
+    :license:   MIT, see LICENSE for more details.
+    :copyright: Copyright (c) 2017 LoRexxar. All rights reserved
+"""
+
+wordpress = {
+    "esc_url": [1000, 10001, 10002],
+    "esc_js": [1000, 10001, 10002],
+    "esc_html": [1000, 10001, 10002],
+    "esc_attr": [1000, 10001, 10002],
+    "esc_textarea": [1000, 10001, 10002],
+    "tag_escape": [1000, 10001, 10002],
+    "esc_sql": [1004, 1005, 1006],
+    "_real_escape": [1004, 1005, 1006],
+}
+
+
+wordpress_controlled = []
