c479c20567d8f1b57a642940144c8548167d0606
LoRexxar <lorexxar@gmail.com>
2020-09-21 11:31:38 +0800

    add some tamper

---
+++ rules/tamper/thinkphp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/14 17:01
+# @Author  : LoRexxar
+# @File    : thinkphp.py
+# @Contact : lorexxar@gmail.com
+
+
+thinkphp = {
+}
+
+thinkphp_controlled = [
+    "Input",
+    "request",
+    "I",
+]
