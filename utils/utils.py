f02817b90196cacc350fcd4ffc8434b6f4013b83
LoRexxar <lorexxar@gmail.com>
2020-12-03 13:51:17 +0800

    fix show content line number

--- utils/utils.py
+++ utils/utils.py
@@ -706,15 +706,17 @@ def file_output_format(content):
 def show_context(filename, line_number, show_line=3):
     filename = check_filepath(PROJECT_DIRECTORY, filename)
     line_start = int(line_number) - show_line
+    line_start = line_start if line_start else 1
     line_end = int(line_number) + show_line
 
     lines = get_line(filename, "{},{}".format(line_start, line_end))
 
     i = 0
     for line in lines:
-        i += 1
 
-        if i == (show_line):
+        if line_start + i == int(line_number):
             logger_console.warning("%4d: %s" % (line_start+i, line.replace("\n", "")))
         else:
             logger_console.info("%4d: %s" % (line_start+i, line.replace("\n", "")))
+
+        i += 1
